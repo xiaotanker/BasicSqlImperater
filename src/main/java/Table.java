423ddfc6d28c1cb8fbfb47babe61a62663f06882
Tianbo Jiang <xiaotanker.oi@gmail.com>
2020-11-27 18:36:39 +0800

    remove chinese comment

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -400,13 +400,13 @@ public class Table {
 
         }
         CharStream stream = CharStreams.fromString(conditions);
-        //用 in 构造词法分析器 lexer，词法分析的作用是产生记号
+
         GrammarLexer lexer = new GrammarLexer(stream);
 
-        //用词法分析器 lexer 构造一个记号流 tokens
+
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        //再使用 tokens 构造语法分析器 parser,至此已经完成词法分析和语法分析的准备工作
+
         GrammarParser parser = new GrammarParser(tokens);
         GrammarParser.ConditionsContext ctx=parser.conditions();
 
